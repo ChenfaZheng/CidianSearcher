50043ca8bfd193d7ce80e6dadbe43a2ba0ad3677
Chenfa Zheng <zhengcf@mail.bnu.edu.cn>
2023-08-22 03:33:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+# 现某汉语词典Searcher
+
+词典是网上找的（https://github.com/bamboo512/ModernChineseDict），mdx分析库来自https://bitbucket.org/xwang/mdict-analysis/src/master/（pureSalsa20.py和readmdict.py和ripemd128.py）
+
+## 安装
+
+基于Python3，依赖于python-lzo与xmltodict
+
+```shell
+pip install python-lzo xmltodict
+```
+然后下载所有文件到工作文件夹即可
+
+## 使用
+
+引入包，确定字典路径
+```python
+import os
+from searchercore import Searcher
+
+MDX_PATH = 'PATH/TO/YOUR/DICT.mdx'
+assert os.path.exists(MDX_PATH)
+```
+`MDX_PATH`为字典MDX格式文件的路径
+
+然后创建一个searcher对象
+```python
+searcher = Searcher(MDX_PATH)
+```
+
+### 搜索模式
+
+将搜索结果打印到屏幕：
+```python
+searcher.search(示例)
+```
+如果搜索不到，将在屏幕提示“No such key”
+
+若想将搜索结果以字符串形式得到，使用`get()`方法：
+```pythoon
+result_string = searcher.get(示例)
+```
+如果搜索不到，将返回字符串`No such key`。`search()`方法是对`get()`方法的封装。
+
+### 遍历模式
+
+使用`search_all()`方法，将字典中所有可用词条展示在屏幕上（很耗时！）
+```python
+searcher.search_all()
+```
+本质是对词库中的所有词条调用`search()`方法
+
+### 手气不错
+
+使用`lucky()`方法，在词典中随机选取一个（或若干个）词条，在屏幕上展示
+```python
+searcher.lucky(6)
+```
+
+## 已知bug
+- “同”、“见”同时出现时，只能正常显示第一个![](image.png)
+- 部分“见下”词条不能正常显示![Alt text](image-1.png)
+
+以上bug均可通过修改源代码，改变词条内容处理方式解决
+
+## License
+
+GPL v3
---
+++ searchercore.py
@@ -0,0 +1,227 @@
+from readmdict import MDX, MDD
+import xmltodict
+from random import randint
+
+
+class Searcher():
+    def __init__(self, mdx_path):
+        self.mdx = MDX(mdx_path)
+        self.items = [*self.mdx.items()]
+        # remove items that hase a key starting with '0'
+        self.items = [x for x in self.items if not x[0].decode().startswith('0')]
+        self.len_items = len(self.items)
+        print('\n-------------------')
+        print(f'{mdx_path} loaded')
+        print(f'Number of items: {self.len_items}')
+        print('-------------------\n')
+    
+    def get(self, mykey, last_key=None) -> str:
+        if last_key == mykey:
+            return
+        
+        have_key = False
+        for k, v in self.items:
+            key = k.decode()
+            if key == mykey:
+                have_key = True
+                break
+        if not have_key:
+            return 'No such key'
+        
+        output_page = '====================\n'
+        val = v.decode()
+        if val.startswith('@@@LINK='):
+            real_key = val[8:].strip()
+            output_page += f'{key} SAME AS {real_key}\n'
+            output_page += self.get(real_key, last_key=mykey)
+            return output_page
+
+        context_lst = []
+        idx0 = -1
+        for idx in range(len(val)):
+            if val[idx:idx+6] == '<entry':
+                idx0 = idx
+            elif val[idx:idx+8] == '</entry>':
+                context_lst.append(val[idx0:idx+8])
+                idx0 = -1
+        
+        # remove image tag
+        for idx, context in enumerate(context_lst):
+            cidx = 0
+            while cidx < len(context):
+                if context[cidx:cidx+4] == '<img':
+                    for cidx1 in range(cidx, len(context)):
+                        if context[cidx1] == '>':
+                            break
+                    context = context[:cidx] + context[cidx1+1:]
+                else:
+                    cidx += 1
+            context_lst[idx] = context
+
+        # remove table tag
+        for idx, context in enumerate(context_lst):
+            cidx = 0
+            while cidx < len(context):
+                if context[cidx:cidx+6] == '<table':
+                    for cidx1 in range(cidx, len(context)):
+                        if context[cidx1-7:cidx1+1] == '</table>':
+                            break
+                    context = context[:cidx] + context[cidx1+1:]
+                else:
+                    cidx += 1
+            context_lst[idx] = context
+
+        
+        for context in context_lst:
+            # output_page += f'Context: {context}\n'
+            context_dict = xmltodict.parse(context)
+
+            # deal with title
+            title_str = ''
+            if 'hw' in context_dict['entry']:
+                if type(context_dict['entry']['hw']) is str:
+                    title_str += f'{context_dict["entry"]["hw"]} '
+                else:
+                    title_str += f'{context_dict["entry"]["hw"]["#text"]} '
+                    if 'pinyin' in context_dict['entry']['hw']:
+                        title_str += f'({context_dict["entry"]["hw"]["pinyin"]}) '
+            output_page += f'{title_str}\n'
+
+            # deal with defination
+            def_lst = context_dict['entry']['def']
+            # output_page += f'{def_lst}\n'
+            if type(def_lst) is str:
+                output_page += f'\t{def_lst}\n'
+                continue
+            elif type(def_lst) is dict:
+                def_lst = [def_lst]
+            assert type(def_lst) is list
+            
+
+            for def_ in def_lst:
+                lingjian = False
+                tong = False
+                jian = False
+                output_str = ''
+                output_str += '\t'
+                if type(def_) is str:
+                    output_str += f'{def_}'
+                    output_page += f'{output_str}\n'
+                    continue
+                if 'num' in def_:
+                    output_str += f'{def_["num"]} '
+                if 'pinyin' in def_:
+                    if '#text' in def_ and '另见' in def_['#text']:
+                        lingjian = True
+                        lingjian_str = def_['#text']
+                        lingjian_split = lingjian_str.split('另见')
+                        if len(lingjian_split[-1]) > 0:
+                            output_str += f'另见{def_["pinyin"]}{lingjian_split[-1]} '
+                    else:
+                        output_str += f'({def_["pinyin"]}) '
+                if 'ps' in def_:
+                    output_str += f'[{def_["ps"]}] '
+                if (not lingjian) and ('#text' in def_):
+                    if def_['#text'].startswith('同“”'):
+                        if type(def_['a']) is list:
+                            tmp_str = '、'.join([
+                                f'“{x["@href"][8:]}”' for x in def_['a']
+                            ])
+                            output_str += f'同{tmp_str}{def_["#text"][3:].replace("、", "")} '
+                        else:
+                            output_str += f'同“{def_["a"]["@href"][8:]}”{def_["#text"][3:]} '
+                        tong = True
+                    if def_['#text'].startswith('旧同“”'):
+                        if type(def_['a']) is list:
+                            tmp_str = '、'.join([
+                                f'“{x["@href"][8:]}”' for x in def_['a']
+                            ])
+                            output_str += f'旧同{tmp_str}{def_["#text"][4:].replace("、", "")} '
+                        else:
+                            output_str += f'旧同“{def_["a"]["@href"][8:]}”{def_["#text"][4:]} '
+                        tong = True
+                    elif def_['#text'].startswith('见【】'):
+                        if type(def_['a']) is list:
+                            tmp_str = '、'.join([
+                                f'【{x["@href"][8:]}】' for x in def_['a']
+                            ])
+                            output_str += f'见{tmp_str}{def_["#text"][3:].replace("、", "")} '
+                        else:
+                            output_str += f'见【{def_["a"]["@href"][8:]}】{def_["#text"][3:]} '
+                        jian = True
+                    elif def_['#text'].startswith('见〖〗'):
+                        if type(def_['a']) is list:
+                            tmp_str = '、'.join([
+                                f'〖{x["@href"][8:]}〗' for x in def_['a']
+                            ])
+                            output_str += f'见{tmp_str}{def_["#text"][3:].replace("、", "")} '
+                        else:
+                            output_str += f'见〖{def_["a"]["@href"][8:]}〗{def_["#text"][3:]} '
+                        jian = True
+                    elif def_['#text'].startswith('见“”'):
+                        if type(def_['a']) is list:
+                            tmp_str = '、'.join([
+                                f'“"{x["@href"][8:]}”' for x in def_['a']
+                            ])
+                            output_str += f'见{tmp_str}{def_["#text"][3:].replace("、", "")} '
+                        else:
+                            output_str += f'见“"{def_["a"]["@href"][8:]}”{def_["#text"][3:]} '
+                        jian = True
+                    else:
+                        output_str += f'{def_["#text"]} '
+                if 'ex' in def_:
+                    if type(def_['ex']) is list:
+                        tmp_str = ''
+                        for ex in def_['ex']:
+                            if type(ex) is str:
+                                tmp_str += f'{ex}'
+                            elif type(ex) is dict:
+                                tmp_str += f'{ex["#text"]} '
+                        output_str += f'{tmp_str} '
+                    elif type(def_['ex']) is dict:
+                        output_str += f'{def_["ex"]["#text"]} '
+                    else:
+                        output_str += f'{def_["ex"]} '
+                
+                output_page += f'{output_str}\n'
+        
+        # after processing
+        if '()' in output_page:
+            output_page = output_page.replace('()', '')
+        if '[]' in output_page:
+            output_page = output_page.replace('[]', '')
+        if '（）' in output_page:
+            output_page = output_page.replace('（）', '')
+        if '【】' in output_page:
+            output_page = output_page.replace('【】', '')
+        if '〖〗' in output_page:
+            output_page = output_page.replace('〖〗', '')
+        return output_page
+    
+    def search(self, mykey) -> None:
+        res = self.get(mykey)
+        if res == 'No such key':
+            print(f'No such key: {mykey}')
+        else:
+            print(res)
+
+    def search_all(self) -> None:
+        for k, v in self.items:
+            key = k.decode()
+            print(f'Key: {key}')
+            self.search(key)
+
+    def lucky(self, num=1) -> None:
+        assert num <= self.len_items, f'Number of items is {self.len_items}, but you want {num}'
+        # generate random number without repetition
+        random_lst = []
+        while len(random_lst) < num:
+            random_num = randint(0, self.len_items-1)
+            if random_num not in random_lst:
+                random_lst.append(random_num)
+        # search for keys at those random numbers
+        for random_num in random_lst:
+            k, v = self.items[random_num]
+            key = k.decode()
+            print(f'Key: {key}')
+            self.search(key)
